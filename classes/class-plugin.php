1ea25ebb18fdfc61905e047be6d7b8d2c8079387
Christian <me@kisu.li>
2021-04-16 17:40:38 +0300

    Well maybe don't always destroy history

--- classes/class-plugin.php
+++ classes/class-plugin.php
@@ -439,8 +439,10 @@ class Plugin {
 
         $this->io->db->updateFormSubmissionsTable($form, $newFields, $deletedFields);
 
-        // The history of the form also has to go. Otherwise the field names stay reserved.
-        $this->io->db->destroyHistoryFields($form);
+        if ($deletedFields) {
+          // The history of the form also has to go. Otherwise the field names stay reserved.
+          $this->io->db->destroyHistoryFields($form);
+        }
       }
     } catch (Error $e) {
       $msg = $e->getMessage();
